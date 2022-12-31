e0e76ae976c4d1c2c2655759b15e0b86ff9a09cb
AlexRixten <rixten.09@gmail.com>
2022-12-31 12:30:02 +0300

    fix store

--- src/store/app/store.js
+++ src/store/app/store.js
@@ -1,10 +1,11 @@
 import { configureStore } from "@reduxjs/toolkit";
-import postsReducer from "./posts/postsSlice";
-import usersReducer from "./users/usersSlice";
+import { apiSlice } from "../../api/apiSlice";
 
 export const store = configureStore({
   reducer: {
-    posts: postsReducer,
-    users: usersReducer, 
+    [apiSlice.reducerPath]: apiSlice.reducer,
   },
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware().concat(apiSlice.middleware),
+  devTools: true,
 });
