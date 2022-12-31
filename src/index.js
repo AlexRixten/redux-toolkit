5b1e713a9916c074a2d898bfb2b5b0524e2ac812
AlexRixten <rixten.09@gmail.com>
2022-12-31 12:30:42 +0300

    fix

--- src/index.js
+++ src/index.js
@@ -4,12 +4,12 @@ import "./index.css";
 import { App } from "./App";
 import { store } from "./store/app/store";
 import { Provider } from "react-redux";
-import { fetchPosts } from "./store/app/posts/postsSlice";
-import { fetchUsers } from "./store/app/users/usersSlice";
+import { extendedApiSlice } from "./store/app/posts/postsSlice";
+import { usersApiSlice } from "./store/app/users/usersSlice";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
-store.dispatch(fetchPosts());
-store.dispatch(fetchUsers());
+store.dispatch(extendedApiSlice.endpoints.getPosts.initiate());
+store.dispatch(usersApiSlice.endpoints.getUsers.initiate());
 
 ReactDOM.render(
   <React.StrictMode>
